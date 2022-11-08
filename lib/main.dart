f3099c016eddc7efa060ec958410dae9e3454f06
Madhi <madhialagan@zaigoinfotech.com>
2022-11-08 14:16:21 +0530

    android notification

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'home_page.dart';
 
 void main(){
